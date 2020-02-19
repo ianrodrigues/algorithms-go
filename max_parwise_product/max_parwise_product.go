0e3bcdc084f1fb93e7af04344d74bb1c8019ff2f
Ian Rodrigues <ianrdgs@gmail.com>
2020-02-19 12:08:38 -0300

    Import MaxParwiseProduct

--- max_parwise_product/max_parwise_product.go
+++ max_parwise_product/max_parwise_product.go
@@ -1,16 +1,9 @@
 package max_parwise_product
 
+import "sort"
+
+// MaxParwiseProduct returns the highest product of 2 items in the given list (n).
 func MaxParwiseProduct(n []int) int {
-	var result int
-
-	for i := 0; i < len(n); i++ {
-		for j := i + 1; j < len(n); j++ {
-			p := n[i] * n[j]
-			if p > result {
-				result = p
-			}
-		}
-	}
-
-	return result
+	sort.Sort(sort.IntSlice(n))
+	return n[len(n)-1] * n[len(n)-2]
 }
