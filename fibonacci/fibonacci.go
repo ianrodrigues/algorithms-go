c004e8ab530c56f5b020073059601ea09f5ebc5f
Ian Rodrigues <ianrdgs@gmail.com>
2020-02-19 10:42:09 -0300

    Fibonacci without recursion

--- fibonacci/fibonacci.go
+++ fibonacci/fibonacci.go
@@ -4,5 +4,12 @@ func Fibonacci(n int) int {
 	if n < 2 {
 		return n
 	}
-	return Fibonacci(n-1) + Fibonacci(n-2)
+
+	f := [2]int{0, 1}
+
+	for i := 2; i <= n; i++ {
+		f[0], f[1] = f[1], f[0]+f[1]
+	}
+
+	return f[1]
 }
